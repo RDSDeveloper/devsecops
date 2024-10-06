3639d33b928cce7eb5df79a7aa136002169b42cc
Rodrigo Damian Santos <RDSDeveloperContact@gmail.com>
2024-10-06 19:50:41 -0300

    Added SA credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,6 +41,7 @@ resource "google_artifact_registry_repository" "docker_repo" {
   location = var.region
   repository_id = "fastapi-repo"
   format = "DOCKER"
+  project = var.project_id  
 }
 
 resource "google_cloud_run_service" "fastapi_service" {
