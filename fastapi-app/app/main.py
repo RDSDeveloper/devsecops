dbf8dcaf54d8c421c6cafa492dc1f41117c3ba6c
Rodrigo Damian Santos <RDSDeveloperContact@gmail.com>
2024-10-07 01:46:38 -0300

    Fixing cloud run

--- fastapi-app/app/main.py
+++ fastapi-app/app/main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from google.cloud import bigquery
 
 app = FastAPI()
 
 @app.get("/data")
 async def get_data():
-    client = bigquery.Client()
-    query = """
-        SELECT * FROM `project_id.analytics_dataset.data_table`
-    """
-    query_job = client.query(query)
-    results = query_job.result()
-    return [dict(row) for row in results]
+    try:
+        client = bigquery.Client()
+        query = """
+            SELECT * FROM `devsecops-437822.analytics_dataset.data_table`
+        """
+        query_job = client.query(query)
+        results = query_job.result()
+        return [dict(row) for row in results]
+    except Exception as e:
+        # Log the error and raise an HTTP exception
+        print(f"Error fetching data: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
